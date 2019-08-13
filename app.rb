f58aec0a87869d71bf4aa506a5c833c3aa0be8f1
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-13 14:36:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,12 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "765u81yifqberewa780fgte43vbrt"
   end
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    
+  end
+
 end
